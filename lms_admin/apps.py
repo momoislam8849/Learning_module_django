ba5ad700497f98a1fc6e6939676c843c6d9e6af5
momoislam8849 <shampa15-8849@diu.edu.bd>
2020-09-19 13:22:30 +0600

    initial files

---
+++ lms_admin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LmsAdminConfig(AppConfig):
+    name = 'lms_admin'
---
+++ lms_main/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LmsMainConfig(AppConfig):
+    name = 'lms_main'
